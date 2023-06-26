c09ccf58d6a19a2f99882a8507beafd3efeb8d0b
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-06-26 12:52:58 -0700

    Wrote function to generate the 5 day forecast ahead, added a reload to the fetch function to clear out the search bar and ensure that the forecast data doesn't interfere. added a function to check local storage and if there is no data then it will run the fetch function by default so the page isn't blank

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,16 @@ var featuredButton = $(".featured-city-btn");
 var userValue = "";
 
 var fetchCurrentWeather = function (userValue) {
-  let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userValue + ",us&appid=eeb8257b7635102394d6d47739f9033b";
+  let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userValue + ",us&units=imperial&appid=eeb8257b7635102394d6d47739f9033b";
   fetch(apiURL)
     .then(function (response) {
+      if (response.status === 200){
+        fetch5dayWeather(userValue);
+      }
       return response.json();
     })
     .then(function (currentWeather) {
       localStorage.setItem("currentWeather", JSON.stringify(currentWeather));
-      console.log(currentWeather)
     })
 } 
 
@@ -25,28 +27,50 @@ var fetch5dayWeather = function (userValue) {
     })
     .then(function (weeklyData) {
       localStorage.setItem("weeklyWeather", JSON.stringify(weeklyData));
-      console.log(weeklyData)
-      $(".search-input").val("");
-      
-      for (let i = 3; i < weeklyData.list.length; i = i + 8){
-      }
+      location.reload();
     });
 };
-// passing in the variable doesn't work here
+
+var writeForecast = function () {
+let currentWeather = JSON.parse(localStorage.getItem("currentWeather"))
+let weeklyWeather = JSON.parse(localStorage.getItem("weeklyWeather"))
+let n = 0;
+for (let i = 3; i < weeklyWeather.list.length; i = i + 8){
+  n++;
+  let dateCreate = document.createElement("h5");
+  dateCreate.setAttribute("class", "card-title forecast-title date")
+  dateCreate.textContent = weeklyWeather.list[i].dt_txt.split(" ")[0]
+  let tempCreate = document.createElement("h6");
+  tempCreate.setAttribute("class", "forecast-title")
+  tempCreate.textContent = "Temp: " + weeklyWeather.list[i].main.temp + "°F";
+  let windCreate = document.createElement("h6");
+  windCreate.setAttribute("class", "forecast-title")
+  windCreate.textContent = "Wind: " + weeklyWeather.list[i].wind.speed + " mph";
+  let humidityCreate = document.createElement("h6");
+  humidityCreate.setAttribute("class", "forecast-title")
+  humidityCreate.textContent = "Humidity: " + weeklyWeather.list[i].main.humidity + "%";
+  $("#Day-" + n).append(dateCreate, tempCreate, windCreate, humidityCreate);
+}
+}
+
+
 searchButton.on("click", function (event) {
   event.preventDefault();
   userValue = $(".search-input").val();
   userValue = userValue.toLowerCase();
   userValue = userValue.replace(" ", "+");
-  localStorage.setItem("city", userValue);
-  console.log(userValue, "hello");
   fetchCurrentWeather(userValue);
-  fetch5dayWeather(userValue);
 });
-// passing in the variable works here
 featuredButton.on("click", function(){
     userValue = $(this).val();
     console.log(userValue);
     fetchCurrentWeather(userValue);
-    fetch5dayWeather(userValue);
 })
+
+if(localStorage.getItem("weeklyWeather")){
+  writeForecast();
+} else if (!localStorage.getItem("weeklyWeather")) {
+  userValue = "los+angeles"
+  fetchCurrentWeather(userValue);
+}
+console.log(JSON.parse(localStorage.getItem("currentWeather")));
