6f2f5227c3bfa1001c5d1ed651fdfd308f76d4f2
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-06-26 21:36:48 -0700

    added comments to the JS file to describe the functionality and rationale of the functions used and what the intended result is

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,7 @@ var fetchCurrentWeather = function (userValue) {
     userValue +
     ",us&units=imperial&appid=eeb8257b7635102394d6d47739f9033b";
   fetch(apiURL)
+  // the function checks to make sure that the response is good, if it isn't it alerts the user to search for a correct City name
     .then(function (response) {
       if (response.status === 200) {
         fetch5dayWeather(userValue);
@@ -22,7 +23,7 @@ var fetchCurrentWeather = function (userValue) {
       localStorage.setItem("currentWeather", JSON.stringify(currentWeather));
     });
 };
-
+// the 5 day forecast function will only run if the response from the first fetch function is good.
 var fetch5dayWeather = function (userValue) {
   let apiURL =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
@@ -37,39 +38,47 @@ var fetch5dayWeather = function (userValue) {
       writeForecast();
       generateButton();
       $(".search-input").val("");
-      // location.reload();
     });
 };
-
+// this function handles the creation of all elements displayed for the forecast. all of information in the 5 day forecast and current day forecast is dynamically generated.
 const writeForecast = function () {
   now = dayjs().format("YYYY-MM-DD");
   let currentWeather = JSON.parse(localStorage.getItem("currentWeather"));
   let weeklyWeather = JSON.parse(localStorage.getItem("weeklyWeather"));
+  // this variable is used to point to the 5 different cards used for the 5 day forecast, the integer is incremented by 1 to loop through all 5 cards
   let n = 0;
+  //the following blocks of code in the for loop is used to write the 5 day forecast to the DOM. the variables are named to match the information that is being grabbed from the forecast data
   for (let i = 3; i < weeklyWeather.list.length; i = i + 8) {
     n++;
+    
+    let dateCreate = document.createElement("h5");
+    dateCreate.setAttribute("class", "card-title forecast-title date");
+    dateCreate.textContent = weeklyWeather.list[i].dt_txt.split(" ")[0];
+    
     let iconCode = weeklyWeather.list[i].weather[0].icon;
     let iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
     let icon = document.createElement("img");
     icon.setAttribute("src", iconUrl);
-    let dateCreate = document.createElement("h5");
-    dateCreate.setAttribute("class", "card-title forecast-title date");
-    dateCreate.textContent = weeklyWeather.list[i].dt_txt.split(" ")[0];
+   
     let descriptionCreate = document.createElement("h6");
     descriptionCreate.setAttribute("class", "forecast-title");
     descriptionCreate.textContent =
       weeklyWeather.list[i].weather[0].description;
+   
     let tempCreate = document.createElement("h6");
     tempCreate.setAttribute("class", "forecast-title");
     tempCreate.textContent = "Temp: " + weeklyWeather.list[i].main.temp + " °F";
+   
     let windCreate = document.createElement("h6");
     windCreate.setAttribute("class", "forecast-title");
     windCreate.textContent =
       "Wind: " + weeklyWeather.list[i].wind.speed + " mph";
+    
     let humidityCreate = document.createElement("h6");
     humidityCreate.setAttribute("class", "forecast-title");
     humidityCreate.textContent =
       "Humidity: " + weeklyWeather.list[i].main.humidity + "%";
+    // empty() is used to clear out the card and replace it with any new forecast data. the function will loop to clear out all 5 day cards
     $("#Day-" + n).empty();
     $("#Day-" + n).append(
       dateCreate,
@@ -80,23 +89,29 @@ const writeForecast = function () {
       humidityCreate
     );
   }
-
+  // out of the for loop, the remaining blocks of code are used to write the info for the current day forecast to the DOM. the variables are named to match the information that is being grabbed from the current day forecast data
+  let currentCity = document.createElement("h5");
+  currentCity.setAttribute("class", "card-title current-date");
+  currentCity.textContent = currentWeather.name + " (" + now + ")";
+  
   let iconCode = currentWeather.weather[0].icon;
   let iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
   let icon = document.createElement("img");
   icon.setAttribute("src", iconUrl);
-  let currentCity = document.createElement("h5");
-  currentCity.setAttribute("class", "card-title current-date");
-  currentCity.textContent = currentWeather.name + " (" + now + ")";
+  
   let descriptionCreate = document.createElement("h6");
   descriptionCreate.textContent = currentWeather.weather[0].description;
+  
   let tempCreate = document.createElement("h6");
   tempCreate.textContent = "Temp: " + currentWeather.main.temp + " °F";
+  
   let windCreate = document.createElement("h6");
   windCreate.textContent = "Wind: " + currentWeather.wind.speed + " mph";
+  
   let humidityCreate = document.createElement("h6");
   humidityCreate.textContent =
     "Humidity: " + currentWeather.main.humidity + "%";
+  // empty() is used to clear out the card and replace it with any new weather data
   $(".city-container").empty();
   $(".city-container").append(
     currentCity,
@@ -107,7 +122,7 @@ const writeForecast = function () {
     humidityCreate
   );
 };
-
+// this function grabs the city name that is saved to the local storage and creates a history of searched cities on the webpage, the city buttons are clickable and will run the function for the provided city name. the buttons are prepended to the parent div so that the most recent search is at the top.
 const generateButton = function () {
   userValue = localStorage.getItem("city");
   buttonCreate = document.createElement("button");
@@ -116,12 +131,12 @@ const generateButton = function () {
   buttonCreate.textContent = userValue.replace("+", " ");
   $(".featured-card-body").prepend(buttonCreate);
 };
-
 // the event listener for the search bar is set grab the value entered by the user and if the search bar is left blank then the function is killed. if the search bar is not blank then it proceeds to run the functions and try to getch the forecast data.
 searchButton.on("click", function (event) {
   event.preventDefault();
   userValue = $(".search-input").val();
   if (userValue === ""){
+    alert("Please enter a valid City");
     return;
   } else {
   userValue = userValue.toLowerCase();
